be79d7592fd81e13103e553a86b9aa68a0d90356
kelvinhsieh28 <kelvinhsieh28@gmail.com>
2025-06-19 02:39:30 +0800

    💡 add RSVP log for debugging

--- server/index.js
+++ server/index.js
@@ -16,7 +16,8 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // --- Express 應用程式設定 ---
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
+
 
 app.use(cors());
 app.use(express.json());
