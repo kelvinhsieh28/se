cc1b0c1ba3df7ae844c18b6487f68003ba3dcc7d
kelvinhsieh28 <kelvinhsieh28@gmail.com>
2025-06-20 00:28:10 +0800

    安裝 multer 與 csv-parser 支援 CSV 上傳

--- server/index.js
+++ server/index.js
@@ -23,6 +23,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // ✅ 加上這一行來支援 multipart/form-data
 app.use(express.static(path.join(__dirname, "../public")));
+app.use('/templates', express.static(path.join(__dirname, '../templates')));
+
 
 // ✅ 資料庫連線
 const db = mysql.createConnection({
