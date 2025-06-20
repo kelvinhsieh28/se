4b9afdad05984b5d749d999e28cce5a5c890d387
hcc02 <joe911224@gmail.com>
2025-06-20 23:02:25 +0800

    批量生產

--- server/index.js
+++ server/index.js
@@ -39,8 +39,8 @@ const db = mysql.createConnection({
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
 });
 
