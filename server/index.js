b1eb1fdd0954b4b651df4755c6bf6ffc595f2b91
hcc02 <joe911224@gmail.com>
2025-06-20 20:06:33 +0800

    批量生產

--- server/index.js
+++ server/index.js
@@ -21,7 +21,8 @@ const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // ✅ 加上這一行來支援 multipart/form-data
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.static(path.join(__dirname, "../public")));
 app.use('/templates', express.static(path.join(__dirname, 'public/templates')));
 
